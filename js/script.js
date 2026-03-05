14ba63d45ee4d3ab361882dce52ec02bc29cd574
amandapacchacommunity <pacchaamanda@gmail.com>
2026-03-05 14:39:36 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,13 +9,15 @@ fetch('data/risk_register.json')
       const card = document.createElement("div");
       card.className = "card";
 
-      card.innerHTML = `
-        <h3>${risk.risk}</h3>
-        <p><strong>Impact:</strong> ${risk.impact}</p>
-        <p><strong>Probability:</strong> ${risk.probability}</p>
-        <p><strong>Owner:</strong> ${risk.owner}</p>
-        <p><strong>Mitigation:</strong> ${risk.mitigation}</p>
-      `;
+     card.innerHTML = `
+  <h3>
+    <a href="case-studies/erm-risk-register.html">${risk.risk}</a>
+  </h3>
+  <p><strong>Impact:</strong> ${risk.impact}</p>
+  <p><strong>Probability:</strong> ${risk.probability}</p>
+  <p><strong>Owner:</strong> ${risk.owner}</p>
+  <p><strong>Mitigation:</strong> ${risk.mitigation}</p>
+`;
 
       container.appendChild(card);
 
