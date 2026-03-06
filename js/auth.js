c56d43bc5b6f31f4cd2ca601617fa9b739ec4c9b
amandapacchacommunity <pacchaamanda@gmail.com>
2026-03-05 22:05:54 -0500

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,10 @@
+const ACCESS_CODE = "risklab2026";
 
+(function protectPage() {
+  const isLoginPage = window.location.pathname.includes("login.html") || window.location.pathname.endsWith("/");
+  const access = sessionStorage.getItem("access");
+
+  if (!isLoginPage && access !== "granted") {
+    window.location.href = "/login.html";
+  }
+})();
